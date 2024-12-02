dafbd0d6db537e15a5e67aa3579d53b0e2172bf8
Sasha Vishnevetskaya <visneveckaya.a@gmail.com>
2024-12-02 18:20:14 +0300

    feat: 4 random cards

--- christmas-shop/index.js
+++ christmas-shop/index.js
@@ -57,3 +57,163 @@ function setClock(endtime) {
 }
 setClock(deadline)
 
+// карточки
+const giftsCardsBlock = document.querySelector(".gifts__cards"); // блок с карточками
+giftsCardsBlock.innerHTML = ""
+
+// добавление всех карточек
+function addCards() {
+  // рандомный index
+  let index1 = Math.floor(Math.random() * 36) + 1;
+  let index2 = Math.floor(Math.random() * 36) + 1;
+  let index3 = Math.floor(Math.random() * 36) + 1;
+  let index4 = Math.floor(Math.random() * 36) + 1;
+  // console.log(index1, index2, index3, index4)
+
+  if (index1 === index2 || index1 === index3 || index1 === index4) {
+    index1 += 1
+  } else if (index2 === index1 || index2 === index3 || index2 === index4) {
+    index2 += 1
+  } else if (index3 === index1 || index3 === index2 || index3 === index4) {
+    index3 += 1
+  } else if (index4 === index1 || index4 === index2 || index4 === index3) {
+    index3 += 1
+  }
+  //console.log(index1, index2, index3, index4)
+
+  if (index1 === index2 || index1 === index3 || index1 === index4) {
+    index1 += 2
+  } else if (index2 === index1 || index2 === index3 || index2 === index4) {
+    index2 += 2
+  } else if (index3 === index1 || index3 === index2 || index3 === index4) {
+    index3 += 2
+  } else if (index4 === index1 || index4 === index2 || index4 === index3) {
+    index3 += 2
+  }
+
+  //console.log(index1, index2, index3, index4)
+
+  giftCards.forEach(item => {
+    if(item.id == index1 || item.id == index2 || item.id == index3 || item.id == index4 ) {
+      giftsCardsBlock.innerHTML += `
+    <li class="gifts__card" data-id=${item.id}>
+      <img class="gifts__card-img" src="${item.img}" alt="${item.category}">
+      <div class="gifts__card-text-content">
+        <h3 class="gifts__card-title gifts__card-title_${item.class}">${item.category}</h3>
+        <p class="gifts__card-text">${item.name}</p>
+      </div>
+    </li>
+  `
+    }
+    
+  })
+  addPetItemClickHandler()
+}
+
+addCards()
+
+// модалка
+let popup = document.createElement("div");
+popup.classList.add("popup");
+document.querySelector("body").append(popup)
+
+function renderPopup(id) {
+  giftCards.forEach(item => {
+    if (item == id) {
+      popup.innerHTML = `
+      <div class="modal" data-id=${item.id}>
+        <div class="modal__wrapper">
+          <div class="modal__content">
+            <img src="./img/close.png" alt="close" class="modal__close">
+            <div class="modal__content-block">
+              <img src="${item.img}" alt="${item.category}" class="modal__img">
+              <div class="modal__text-content">
+                <h3 class="gifts__card-title gifts__card-title_${item.class}">${item.category}</h3>
+                <p class="gifts__card-text">${item.name}</p>
+                <p class="modal__description">${item.description}</p>
+              </div>
+              <div class="modal__superpowers-content">
+                <p class="modal__superpowers-title">Adds superpowers to:</p>
+                <div class="modal__superpowers">
+                  <div class="modal__superpowers-item">
+                    <p class="modal__superpowers-item-name">Live</p>
+                    <p class="modal__superpowers-item-count">${item.superpowers.live}</p>
+                    <div class="modal__superpowers-item-snowflakes">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.live[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.live[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.live[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake modal__superpowers-item-snowflakes_color${item.superpowers.live[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.live[1]}">
+                    </div>
+                  </div>
+                  <div class="modal__superpowers-item">
+                    <p class="modal__superpowers-item-name">Create</p>
+                    <p class="modal__superpowers-item-count">${item.superpowers.create}</p>
+                    <div class="modal__superpowers-item-snowflakes modal__superpowers-item-snowflakes_color">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.create[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.create[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.create[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.create[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.create[1]}">
+                    </div>
+                  </div>
+                  <div class="modal__superpowers-item">
+                    <p class="modal__superpowers-item-name">Love</p>
+                    <p class="modal__superpowers-item-count">${item.superpowers.love}</p>
+                    <div class="modal__superpowers-item-snowflakes modal__superpowers-item-snowflakes_color">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.love[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.love[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.love[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.love[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.love[1]}">
+                    </div>
+                  </div>
+                  <div class="modal__superpowers-item">
+                    <p class="modal__superpowers-item-name">Dream</p>
+                    <p class="modal__superpowers-item-count">${item.superpowers.dream}</p>
+                    <div class="modal__superpowers-item-snowflakes modal__superpowers-item-snowflakes_color">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.dream[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.dream[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.dream[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.dream[1]}">
+                      <img src="./img/modal-snowflake.svg" alt="snowflake" class="modal__superpowers-item-snowflake  modal__superpowers-item-snowflakes_color${item.superpowers.dream[1]}">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+    }
+  })
+  document.querySelector(".modal").addEventListener("click", closePopup);
+  
+}
+
+function addPetItemClickHandler() {
+  document.querySelector(".gifts__cards-content").addEventListener("click", (e) => {
+    if(e.target.closest(".gifts__card")) {
+      let clickCardId = e.target.closest(".gifts__card").getAttribute("data-id");
+      // console.log(clickCardId);
+      let clickItemCard = getClicCards(clickCardId);
+      // console.log(clickItemCard);
+      renderPopup(clickItemCard)
+      document.querySelector(".modal").classList.add("modal_active");
+      document.body.classList.add("popup__open");
+    }
+  })
+}
+
+function getClicCards(id) {
+  return giftCards.find(cardItem => cardItem.id == id)
+}
+
+function closePopup(e) {
+  let classes = e.target.classList;
+  if (classes.contains('modal_active') || classes.contains('modal__close')) {
+    document.querySelector('.modal_active').remove();
+    document.body.classList.remove("popup__open");
+  }
+}
